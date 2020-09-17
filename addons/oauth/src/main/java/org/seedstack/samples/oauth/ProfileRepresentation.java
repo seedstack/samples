381cab7235c0b405570aa2d442b08f5ed240ffdc
Adrien LAUER <adrien.lauer@gmail.com>
2020-09-17 18:00:06 +0200

    Add initial oauth sample

---
+++ addons/oauth/src/main/java/org/seedstack/samples/oauth/ProfileRepresentation.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright © 2013-2020, The SeedStack authors <http://seedstack.org>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package org.seedstack.samples.oauth;
+
+import java.util.Map;
+
+public class ProfileRepresentation {
+    private String firstName;
+    private String lastName;
+    private String fullName;
+    private String pictureUrl;
+    private Map<String, String> principals;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getPictureUrl() {
+        return pictureUrl;
+    }
+
+    public void setPictureUrl(String pictureUrl) {
+        this.pictureUrl = pictureUrl;
+    }
+
+    public Map<String, String> getPrincipals() {
+        return principals;
+    }
+
+    public void setPrincipals(Map<String, String> principals) {
+        this.principals = principals;
+    }
+}
---
+++ addons/oauth/src/main/java/org/seedstack/samples/oauth/ProfileResource.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright © 2013-2020, The SeedStack authors <http://seedstack.org>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package org.seedstack.samples.oauth;
+
+import org.seedstack.seed.security.SecuritySupport;
+import org.seedstack.seed.security.principals.Principals;
+import org.seedstack.seed.security.principals.SimplePrincipalProvider;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+@Path("/profile")
+public class ProfileResource {
+    @Inject
+    private SecuritySupport securitySupport;
+
+    @GET
+    @Produces("application/json")
+    public ProfileRepresentation sayHello() {
+        ProfileRepresentation profileRepresentation = new ProfileRepresentation();
+        Optional.ofNullable(securitySupport.getSimplePrincipalByName(Principals.FIRST_NAME))
+                .map(SimplePrincipalProvider::get)
+                .ifPresent(profileRepresentation::setFirstName);
+        Optional.ofNullable(securitySupport.getSimplePrincipalByName(Principals.LAST_NAME))
+                .map(SimplePrincipalProvider::get)
+                .ifPresent(profileRepresentation::setLastName);
+        Optional.ofNullable(securitySupport.getSimplePrincipalByName(Principals.FULL_NAME))
+                .map(SimplePrincipalProvider::get)
+                .ifPresent(profileRepresentation::setFullName);
+        Optional.ofNullable(securitySupport.getSimplePrincipalByName("picture"))
+                .map(SimplePrincipalProvider::get)
+                .ifPresent(profileRepresentation::setPictureUrl);
+
+        Map<String, String> principals = new HashMap<>();
+        for (SimplePrincipalProvider simplePrincipalProvider : securitySupport.getSimplePrincipals()) {
+            principals.put(simplePrincipalProvider.getName(), simplePrincipalProvider.getValue());
+        }
+        profileRepresentation.setPrincipals(principals);
+
+        return profileRepresentation;
+    }
+}
